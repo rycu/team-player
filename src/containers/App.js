2b4e413aef9f42a7759f63515a652b0d27f650ae
rycu <rycuMMXVII@cuttermail.co.uk>
2017-06-01 21:45:29 +0100

    inline svg component added

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
+import IconLogo from '../components/IconLogo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <IconLogo FillColor="orange"/>
           <h2>This will be team-player</h2>
         </div>
         <p>

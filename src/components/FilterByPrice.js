ee7a8a8fed044084edc6affcf6a4e6a8a8de6298
rycu <rycuMMXVII@cuttermail.co.uk>
2017-06-17 12:38:25 +0100

    high/low slider label rounding to 1 point & min range added (gap)

--- src/components/FilterByPrice.js
+++ src/components/FilterByPrice.js
@@ -12,20 +12,29 @@ export default class FilterByPrice extends Component {
 	    highVal: this.props.priceObj.highVal
 	}
 
+	instanceProps = {
+
+		min: 0, 
+		max: 20,
+		step: 0.1,
+		gap: 2
+
+	}
+
 	overlapCorrect(id){
 
 		let lowVal = this.state.lowVal;
 		let highVal = this.state.highVal;
 
-		if(id === 'low' && (lowVal > highVal)) {
+		if(id === 'low' && ((lowVal+this.instanceProps.gap) > highVal)) {
 			this.setState({ 
 				lowVal: lowVal,
-				highVal: lowVal
+				highVal: lowVal+this.instanceProps.gap
 			})
-		}else if(id === 'high' && (highVal < lowVal)){
+		}else if(id === 'high' && ((highVal-this.instanceProps.gap) < lowVal)){
 			this.setState({ 
 				highVal: highVal,
-				lowVal: highVal
+				lowVal: highVal-this.instanceProps.gap
 			})
 		}else if (id !== 'low' && id !== 'high'){
 			console.log('overlapCorrect bad ID: '+ id);
@@ -35,9 +44,20 @@ export default class FilterByPrice extends Component {
 	}
 
 	updateState(id, value){
-		this.setState({ 
-				[id+'Val']: parseFloat(value)
-		})
+
+		let lowMax = this.instanceProps.max-this.instanceProps.gap;
+		let highMin = this.instanceProps.min+this.instanceProps.gap;
+		
+		if(
+			(id === 'low' && value >= this.instanceProps.min && value <= lowMax) ||
+			(id === 'high' && value >= highMin && value <= this.instanceProps.max)
+			){
+			this.setState({ 
+					[id+'Val']: parseFloat(value)
+			})
+		}
+
+
 		this.overlapCorrect(id);
 	}
 
@@ -67,7 +87,13 @@ export default class FilterByPrice extends Component {
 		});
 	}
 
+	lableRound(value){
+		return Math.round( value * 10 ) / 10;
+	}
+
 	render() {
+
+		//console.log(this.test);
 
 		let lowVal = this.state.lowVal;
 		let highVal = this.state.highVal;
@@ -77,9 +103,9 @@ export default class FilterByPrice extends Component {
 				<input 
 					id="low" 
 					type="range" 
-					min="0" 
-					max="20" 
-					step="0.1"
+					min={this.instanceProps.min} 
+					max={this.instanceProps.max} 
+					step={this.instanceProps.step}
 					value={lowVal}
 					onChange={this.handleChange} 
 					onMouseUp={this.handleSubmit}
@@ -89,16 +115,16 @@ export default class FilterByPrice extends Component {
 				<input 
 					id="high" 
 					type="range" 
-					min="0" 
-					max="20" 
-					step="0.1"
+					min={this.instanceProps.min} 
+					max={this.instanceProps.max} 
+					step={this.instanceProps.step} 
 					value={highVal}
 					onChange={this.handleChange} 
 					onMouseUp={this.handleSubmit}
 					onKeyUp={this.handleSubmit}
 				/>
 
-				<label>{lowVal}M to {highVal}M</label>
+				<label>{this.lableRound(lowVal)}M to {this.lableRound(highVal)}M</label>
 
 			</div>
     	);
